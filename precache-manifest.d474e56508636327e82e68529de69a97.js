486af1896d705ea48f472d5c7d4b749b987d03d0
thalesleite <thalesaleite@gmail.com>
2020-02-09 11:41:47 +0000

    Updates

---
+++ precache-manifest.d474e56508636327e82e68529de69a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e815d56d403569970715d62437bfc8",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "cc244c01c8c30c75de08",
+    "url": "/monsters-roledex/static/css/main.012f6f9c.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-roledex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-roledex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc244c01c8c30c75de08",
+    "url": "/monsters-roledex/static/js/main.8c97608d.chunk.js"
+  },
+  {
+    "revision": "25b7faa79a9dd952bd80",
+    "url": "/monsters-roledex/static/js/runtime-main.64669627.js"
+  }
+]);
